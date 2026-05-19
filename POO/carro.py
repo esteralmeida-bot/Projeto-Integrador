8968c57b28cbf8964825936688dbc178a1f4aedd
Ester Abreu Martins de Almeida <202511900029@ifto.local>
2026-05-19 16:32:57 -0300

    Subindo arquivos

--- POO/carro.py
+++ POO/carro.py
@@ -1,21 +1,34 @@
 # A palavra "class" é usada para criar uma classe.
-# Uma classe funciona como um molde para criar objetos 
-class Carro: 
-    # Def: defini função ou método.
-    # Init é o metodo construtor da classe
+# Uma classe funciona como um molde para criar objetos
+class Carro:
+
+    # "def" definir uma função ou método.
+    # "__init__" é o método construtor da classe.
     # Ele é executado automaticamente quando um objeto é criado
-    # self representa o próprio objeto.
-    # é atraves do self que acessamos atributos e metodos do objeto
-    # "marca, modelo, ano, e velocidade" são parametros recebidos pela classe.
-    def __init__(self, marca, modelo, ano, velocidade,=0)
+
+    # "self" representa o próprio objeto.
+    # É através do self que acessamos atributos e métodos do objeto
+
+    # "marca", "modelo", "ano" e "velocidade"
+    # São Parâmetros recebidos pela classe.
+
+    def __init__(self, marca, modelo, ano, velocidade=0):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.velocidade = velocidade
-    # Criando um objeto da classe carro 
-    # carro1 é uma variavel que recebe um objeto
-    carro1 = Carro("Chevrolet", "S10", 2013)
 
-    #exibir informações do carro
-    print(f"Marca: {self.marca}")
-    
+# Criando um objeto da Classe Carro
+# "carro1" é uma variável que recebe um objeto
+carro1 = Carro("Chevrolet", "S10", 2013)
+
+# Exibir informações do carro
+print(f"Marca: {carro1.marca}")
+print(f"Modelo: {carro1.modelo}")
+print(f"Ano: {carro1.ano}")
+
+carro2 = Carro("Civic", "G10", 2021)
+print(f"Marca: {carro2.marca}")
+print(f"Modelo: {carro2.modelo}")
+print(f"Ano: {carro2.ano}")
+
